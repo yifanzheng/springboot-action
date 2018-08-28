85857844ff91d3df2380fa5d79a9b83b695ff676
kevin600 <1561741669@qq.com>
2018-08-28 20:31:18 +0800

    Springboot使用devtools进行热部署

---
+++ springboot-devtools/src/main/java/com/example/devtools/DevtoolsApplication.java
@@ -0,0 +1,12 @@
+package com.example.devtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtoolsApplication.class, args);
+	}
+}
