2d503581eb2205d2d01393b4963837818b151c55
kevin600 <1561741669@qq.com>
2018-09-05 21:38:16 +0800

    ..

--- core-service2.0.0/src/main/java/com/newegg/core/service/web/rest/package-info.java
+++
@@ -1,5 +0,0 @@
-
-/**
- * Spring MVC REST controllers
- */
-package com.newegg.core.service.web.rest;
--- core-service2.0.0/src/main/java/com/newegg/core/service/web/rest/util/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Utils by Spring MVC REST controllers.
- */
-package com.newegg.core.service.web.rest.util;
