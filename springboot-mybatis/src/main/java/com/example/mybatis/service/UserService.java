d5390876991110e4e86ea46733da65f96b4bf399
kevin600 <1561741669@qq.com>
2018-10-22 20:38:32 +0800

    springboot整合mybatis，使用@transaction进行事务管理

--- springboot-mybatis/src/main/java/com/example/mybatis/service/UserService.java
+++ springboot-mybatis/src/main/java/com/example/mybatis/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.mybatis.domain.Users;
 import com.example.mybatis.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -21,6 +22,7 @@ public class UserService {
     /**
      * 根据用户名获取信息
      */
+    @Transactional(propagation = Propagation.SUPPORTS)
     public Users getUser(String name){
         Users user = mapper.findByName(name);
         return user;
@@ -30,7 +32,7 @@ public class UserService {
     /**
      * 添加用户数据
      */
-    @Transactional(rollbackFor = Exception.class)
+    @Transactional(rollbackFor = Exception.class,propagation = Propagation.REQUIRED)
     public int insertUser(String name,String password){
         int insert = mapper.insert(name, password);
         // int a=1/0;
